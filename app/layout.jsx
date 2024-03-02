8a941b7804f5380500daba73afd3dbaad3ae800b
manurik95 <142829913+manurik95@users.noreply.github.com>
2024-03-02 19:45:52 +0100

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import "@styles/globals.css";
+import Navbar from "../components/Navbar";
+import Provider from "../components/Provider";
+
+export const metadata = {
+  title: "Pomptopia",
+  description: "Discover and Share AI Propmpts",
+};
+
+const RootLayout = ({ children }) => (
+  <html lang="en">
+    <body>
+      <Provider>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">
+          <Navbar />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
